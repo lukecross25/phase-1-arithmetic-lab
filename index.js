7c9ce9dda16d2be061c6ef90984121f88e6116f8
Luke Cross <luke.cross25@gmail.com>
2022-09-20 16:16:08 -0600

    commit tree 95edb06531e233ac627eac21d0943d9bf79bc0dd

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+var num1 = 31
+var num2 = 2
+let multiply = num1*num2
+let random = Math.floor(Math.random() * 10) + 1;
+var num3 = 12
+var num4 = 3
+let mod = num3/num4
+let max = Math.max(1,2,3,20)
